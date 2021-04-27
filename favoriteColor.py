1862fb86e00c0ac68b2ef7be019b84fb2321e4a3
Ujeph5 <j.s.alexander.v@gmail.com>
2021-04-27 18:56:32 -0300

    jamesbond

---
+++ favoriteColor.py
@@ -0,0 +1,4 @@
+name = input("What is your best friend's name?")
+hometown = input("Where is he/she from?")
+color = input("What is his/her favorite color?")
+print(name + " from " + hometown + ", loves the color " + color + "!")
